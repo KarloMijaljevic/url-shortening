e2b125d0d8cab3bb922fda3d357d07c44478f84a
KarloMijaljevic <karlo.mijaljevic@gmail.com>
2021-08-28 09:22:37 +0200

    Finished setting up the POST HTTP rule

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 Simple URL shortening app
 1. First step was to set up Linux Ubuntu WSL, node.js and npm.
 2. Second step was to fix readme :D and install express and set up a simple endpoint
+3. Third step is to add a POST HTTP rule for the url shortening service:
+   1. Client sends json = {"url":"anyUrl"}
+   2. Server checks if the URL valid (uses validator package to do so)
+   3. If the URL is valid it caches it using a short url as a unique key and returns 
+      the client a json = {"shortUrl":"shortUrl", "url":"anyUrl"}
+   4. If the URL is not valid send a 400 Bad Request response
+   5. Packages installed: node-cache, randomstring and validator
--- routes/urlProvider.js
+++ routes/urlProvider.js
@@ -1,13 +1,44 @@
 // Import npm packages
 const express = require("express");
+const validator = require("validator");
+const NodeCache = require("node-cache");
+const randomstring = require("randomstring");
 
 // Object declarations
 const router = express.Router();
+const myCache = new NodeCache();
 
-// @route ==> GET url
-// @description ==> Client sends the short URL and server has to respond with a redirect using the CACHed URL
-router.get("/", (req, res) => {
-   res.status(200).json({ "message": "Hello there :)" });
+// @route ==> POST url
+// @description ==> Client sends a URL and server has to respond with a short version and the old version
+router.post("/", (req, res) => {
+   var url = req.body.url;
+   var shortUrl = "";
+   // URL checking and shortening
+   if (validator.isURL(url)) {
+      // OK request response
+      // Simple URL fix, only works for .com and regular www. web pages
+      url = !url.includes("www") ? ("www." + url) : url;
+      url = (!url.includes("https") && !url.includes("http")) ? ("https://" + url) : url;
+      url = !url.includes("com") ? (url + ".com") : url;
+      shortUrl = shortenUrl(url);
+      res.status(200).json({ "shortUrl": shortUrl, "url": url });
+   }
+   else {
+      // Bad request response
+      res.status(400).json({ "error": "Given URL is NOT valid >_<" });
+   }
 })
+
+// @description ==> Used to shorten and cache given URL
+function shortenUrl(url) {
+   // Check if URL is already present in CACHE
+   if (myCache.has(url)) {
+      return myCache.get(url);
+   }
+   var rand = randomstring.generate(4);
+   // CACHE the new short URL with the old one 
+   myCache.set(rand, url);
+   return rand;
+}
 
 module.exports = router
